90641b7d702ab29ebcaeaf6540286044a3ae0560
NilaCSE16 <112371425+NilaCSE16@users.noreply.github.com>
2024-04-03 10:10:15 +0600

    Something

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -39,6 +39,8 @@ const AuthProvider = ({ children }) => {
           method: "POST",
           credentials: "include",
           headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
             "content-type": "application/json",
           },
           body: JSON.stringify(loggedUser),
